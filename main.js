f34ef03ce488e74ae7ed6ea85c82ef446cc29683
Farhad Abideen <abideen2021@gmail.com>
2022-11-26 11:26:48 +0600

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// var fs = require('fs');
-// var http = require('http');
-// var URL = require('url');
 var mongoClient = require('mongodb').MongoClient;
-
 
 
 var config = {useUnifiedTopology: true};
@@ -145,72 +141,3 @@ function findOne(myMongoClient){
     })
 };
 
-// var server = http.createServer(
-//     function (req,res){
-        // res.end('Hello World');
-
-        ////Page Redirection
-        // res.writeHead(200,{'content-type':'text/html'});
-        // if(req.url == "/"){
-        //     res.write('<h1>This is Home Page</h1>');
-        //     res.end();
-        // }else if(req.url == "/contact"){
-        //     res.write('<h1>This is Contact Page</h1>');
-        //     res.end();
-        // }else if(req.url = "/about"){
-        //     res.write('<h1>This is About Page</h1>');
-        //     res.end();
-        // }
-
-        ////URL Parsing
-        // var myUrl = "http://farhad.com.bd/contact.html?year2021";
-        // var myUrlObj = URL.parse(myUrl,true);
-        // var myUrlHostName = myUrlObj.host;
-        // var myUrlPathName = myUrlObj.path;
-        // var myUrlSerach = myUrlObj.search;
-
-        // res.writeHead(200,{'content-type':'text/html'});
-        // res.write(myUrlHostName);
-        // res.end();
-
-        ////Read File (html,pdf,txt etc)
-//         if(req.url == "/"){
-//             fs.readFile('Home.html',function (error,data){
-//                 res.writeHead(200,{'content-type':'text/html'});
-//                 res.write(data);
-//                 res.end();
-//             });
-//         }     
-        
-//         ////File Write asc
-//         if(req.url == "/FileWriteAsc"){
-//             fs.writeFile("note.txt","File write practice Asc", function(error){
-//                 res.writeHead(200,{'content-type':'text/html'});
-//                 if(error){
-//                     res.write("File write fail");
-//                     res.end;
-//                 }else{
-//                     res.write("File write Success");
-//                     res.end;
-//                 }
-//             });
-//         }
-
-//         ////File Write sync
-//         if(req.url == "/FileWriteAsc"){
-//             let error = fs.writeFileSync("note.txt", "File write practice Sync");
-//             res.writeHead(200,{'content-type':'text/html'});
-//             if(error){
-//                 res.write("File write fail");
-//                 res.end;
-//             }else{
-//                 res.write("File write Success");
-//                 res.end;
-//             }
-//         }
-
-//     }
-// );
-
-// server.listen(4040);
-// console.log('Server is running..');
